739f4120379015e1bb2898415331f97f37cfce28
Badmus Emmanuel <badmusemmanuel77@gmail.com>
2024-12-16 01:24:12 +0100

    emmeax

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -31,7 +31,18 @@ const About = () => {
                 </div>
               </div>
               <div className="flex items-center">
-                <label htmlFor="htmlandcss" className="w-2/12">
+                <label htmlFor="javascript" className="w-2/12">
+                  Javascript
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-11/12"
+                  ></div>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <label htmlFor="react" className="w-2/12">
                   React JS
                 </label>
                 <div className="grow bg-gray-800 rounded-full h-2.5">
